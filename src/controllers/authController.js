2b3e99343927a9191f54c8df7b5561f11bff24be
Tarun Jangra <tarunjangra4@gmail.com>
2024-05-03 21:20:00 +0530

    changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,7 @@ const UserModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+// signup api controller
 exports.registerUser = async (req, res) => {
   const authHeaders = req.headers["authorization"];
 
